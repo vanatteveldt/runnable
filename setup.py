984f7515617c3313e32f3e22a11002f004f01f8a
Wouter van Atteveldt <wouter@vanatteveldt.com>
2015-02-09 18:20:23 +0100

    Attempt to provide setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    version='0.1',
+    name="runnable",
+    description="Runnable django forms",
+    author="Wouter van Atteveldt",
+    author_email="wouter@vanatteveldt.com",
+    packages=["runnable"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        "django"
+    ],
+)
